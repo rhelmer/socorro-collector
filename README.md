c5f02d514565c12e0d4a8ee6307ad86473bb9787
Robert Helmer <rhelmer@rhelmer.org>
2015-04-13 12:09:57 -0700

    fix bug 1145703 - split collector out to separate package

---
+++ README.md
@@ -0,0 +1,23 @@
+Socorro Collector
+-----------------
+
+A server for collecting crash reports from clients using the [Breakpad libraries](http://code.google.com/p/google-breakpad/)
+
+Documentation:
+http://socorro.readthedocs.org
+
+Source code:
+https://github.com/mozilla/socorro
+https://github.com/mozilla/socorro-collector
+
+Releases:
+https://github.com/mozilla/socorro/releases
+
+Installation instructions:
+http://socorro.readthedocs.org/en/latest
+
+Socorro mailing list:
+https://lists.mozilla.org/listinfo/tools-socorro
+
+Socorro/Breakpad IRC channel:
+irc://irc.mozilla.org/breakpad
---
+++ setup.py
@@ -0,0 +1,50 @@
+import codecs
+import glob
+import os
+from setuptools import setup, find_packages
+
+
+# Prevent spurious errors during `python setup.py test`, a la
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html:
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+
+def read(fname):
+    fpath = os.path.join(os.path.dirname(__file__), fname)
+    with codecs.open(fpath, 'r', 'utf8') as f:
+        return f.read().strip()
+
+
+setup(
+    name='socorro-collector',
+    version='0.1',
+    description=('Socorro is a server to accept Breakpad'
+                 ' crash reports.'),
+    long_description=open('README.md').read(),
+    author='Mozilla',
+    author_email='socorro-dev@mozilla.com',
+    license='MPL',
+    url='https://github.com/mozilla/socorro-collector',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MPL License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        ],
+    keywords=['socorro', 'breakpad', 'crash', 'reporting', 'minidump',
+              'stacktrace'],
+    packages=find_packages(),
+    install_requires=[], # use peep -r requirements.txt instead
+    entry_points={
+        'console_scripts': [
+                'socorro = socorro.app.socorro_app:SocorroWelcomeApp.run'
+            ],
+        },
+    test_suite='nose.collector',
+    zip_safe=False,
+),
